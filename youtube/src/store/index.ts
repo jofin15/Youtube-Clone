86caeae3a570f72891f2a13327e242effd5c4a19
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-11-25 07:10:19 +0000

    sdfg

--- youtube/src/store/index.ts
+++ youtube/src/store/index.ts
@@ -1,26 +1,32 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-import {InitialState} from "../Types"
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState:InitialState={
-    videos:[],
-    currentPlaying:null,
-    searchTerm:"",
-    searchResults:[],
-    nextPageToken:null,
-    recommendedVideos:[],
+import { InitialState } from "../Types";
 
 
+const initialState: InitialState = {
+  videos: [],
+  currentPlaying: null,
+  searchTerm: "",
+  searchResults: [],
+  nextPageToken: null,
+  recommendedVideos: [],
 };
 
-const YoutubeSlice=createSlice({
-    name:youtubeApp,
-    initialState,
-    reducers:{},
-    extraReducers:(builder)=>{},
+const YoutubeSlice = createSlice({
+  name: "youtubeApp",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+  },
 });
 
-export const store=configureStore({
-    reducer:{
-        youtubeApp:YoutubeSlice.reducer
-    }
-})
+export const store = configureStore({
+  reducer: {
+    youtubeApp: YoutubeSlice.reducer,
+  },
+});
+
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
